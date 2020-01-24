4c26c1469ea7592fad2530433cb4f95db520b4e7
grantxx <59643888+grantxx@users.noreply.github.com>
2020-01-24 20:27:04 +0000

    .

--- test_V1.py
+++ test_V1.py
@@ -11,8 +11,9 @@ import matplotlib.pyplot as plt
 #How to create a random array
 #df = pd.DataFrame(np.random.randint(0,20,size=(20, 2)), columns=list('AB'))
 cols=['Date', 'Time', 'Open','High','Low','Last']
-dfd = pd.read_csv('data/d.csv',skipinitialspace=True, usecols = cols)
+dfd = pd.read_csv('data/4hMFE.csv',skipinitialspace=True, usecols = cols)
 dfd=dfd.tail(78)
+#dfd.reset_index(inplace=True)
 
 # Some dummy data
 x = dfd.index
@@ -23,10 +24,18 @@ slope, intercept = np.polyfit(x, y, 1)
 
 # Create a list of values in the best fit line
 X_deviation = [slope * i + intercept for i in x]
-X_deviation_Upper_band = [(slope * i + intercept)+0.01 for i in x]
-X_deviation_lower_band = [(slope * i + intercept)-0.01 for i in x]
+X_deviation_Upper_band = [(slope * i + intercept)+0.005 for i in x]
+X_deviation_lower_band = [(slope * i + intercept)-0.005 for i in x]
 
 # Plot the best fit line over the actual values
+
+#Set the main figure proportions
+fig = plt.figure(figsize=(15,10))
+fig.subplots_adjust(hspace=0.3)
+ax1 = fig.add_subplot(111)
+
+ax1.scatter(x, y, marker='.')
+
 plt.scatter(x, y)
 plt.plot(x, X_deviation, 'r')
 plt.plot(x, X_deviation_Upper_band, 'g')
