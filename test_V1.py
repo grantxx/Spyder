02830a164fb8b0d8a2bc8613061e843f53b184eb
grantxx <59643888+grantxx@users.noreply.github.com>
2020-01-25 14:57:42 +0000

    .

--- test_V1.py
+++ test_V1.py
@@ -11,24 +11,29 @@ import matplotlib.pyplot as plt
 #How to create a random array
 #df = pd.DataFrame(np.random.randint(0,20,size=(20, 2)), columns=list('AB'))
 cols=['Date', 'Time', 'Open','High','Low','Last']
-dfd = pd.read_csv('data/4hMFE.csv',skipinitialspace=True, usecols = cols)
-dfd=dfd.tail(78)
-#dfd['Slope_Upper']=np.nan
-#dfd.reset_index(inplace=True)
+df = pd.read_csv('data/4hMFE.csv',skipinitialspace=True, usecols = cols)
+df=df.tail(78)
+df.reset_index(drop=False,inplace=True )
 
 # data
-x = dfd.index
-y = dfd['High']
+x = df.index
+y = df['High']
 
 # Find the slope and intercept of the best fit line
 slope, intercept = np.polyfit(x, y, 1)
 
 #Find the slops and plce them into new columns
-dfd['std_Deviation'] = [slope * i + intercept for i in x]
-dfd['Slope_Upper'] = [(slope * i + intercept)+0.005 for i in x]
-dfd['Slope_Lower'] = [(slope * i + intercept)-0.005 for i in x]
-    
+df['std_Deviation'] = [slope * i + intercept for i in x]
 
+#Do some calculation to determine upper and lower extremes
+
+_hdh = (df['High'] - df['std_Deviation']).max()        
+_hdl = (df['std_Deviation']-df['Low']).max()
+
+#Put the slope into the dataframe
+df['Slope_Upper'] = [(slope * i + intercept)+_hdh for i in x]
+df['Slope_Lower'] = [(slope * i + intercept)-_hdl for i in x]
+    
 #Set the main figure proportions
 fig = plt.figure(figsize=(20,10))
 fig.subplots_adjust(hspace=0.3)
@@ -37,9 +42,9 @@ ax1 = fig.add_subplot(111)
 # Plot the best fit line over the actual values
 ax1.scatter(x, y, marker='.')
 plt.scatter(x, y)
-plt.plot(x, dfd['std_Deviation'], 'r')
-plt.plot(x, dfd['Slope_Upper'], 'g')
-plt.plot(x, dfd['Slope_Lower'], 'b')
+plt.plot(x, df['std_Deviation'], 'r')
+plt.plot(x, df['Slope_Upper'], 'g')
+plt.plot(x, df['Slope_Lower'], 'b')
 plt.title(slope)
 plt.show()
 
