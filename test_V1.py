15d3a7d1a946c9df06d867be36d0a52e5b825895
grantxx <59643888+grantxx@users.noreply.github.com>
2020-01-25 09:47:28 +0000

    .

--- test_V1.py
+++ test_V1.py
@@ -13,33 +13,33 @@ import matplotlib.pyplot as plt
 cols=['Date', 'Time', 'Open','High','Low','Last']
 dfd = pd.read_csv('data/4hMFE.csv',skipinitialspace=True, usecols = cols)
 dfd=dfd.tail(78)
+#dfd['Slope_Upper']=np.nan
 #dfd.reset_index(inplace=True)
 
-# Some dummy data
+# data
 x = dfd.index
 y = dfd['High']
 
 # Find the slope and intercept of the best fit line
 slope, intercept = np.polyfit(x, y, 1)
 
-# Create a list of values in the best fit line
-X_deviation = [slope * i + intercept for i in x]
-X_deviation_Upper_band = [(slope * i + intercept)+0.005 for i in x]
-X_deviation_lower_band = [(slope * i + intercept)-0.005 for i in x]
-
-# Plot the best fit line over the actual values
+#Find the slops and plce them into new columns
+dfd['std_Deviation'] = [slope * i + intercept for i in x]
+dfd['Slope_Upper'] = [(slope * i + intercept)+0.005 for i in x]
+dfd['Slope_Lower'] = [(slope * i + intercept)-0.005 for i in x]
+    
 
 #Set the main figure proportions
-fig = plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=(20,10))
 fig.subplots_adjust(hspace=0.3)
 ax1 = fig.add_subplot(111)
 
+# Plot the best fit line over the actual values
 ax1.scatter(x, y, marker='.')
-
 plt.scatter(x, y)
-plt.plot(x, X_deviation, 'r')
-plt.plot(x, X_deviation_Upper_band, 'g')
-plt.plot(x, X_deviation_lower_band, 'b')
+plt.plot(x, dfd['std_Deviation'], 'r')
+plt.plot(x, dfd['Slope_Upper'], 'g')
+plt.plot(x, dfd['Slope_Lower'], 'b')
 plt.title(slope)
 plt.show()
 
